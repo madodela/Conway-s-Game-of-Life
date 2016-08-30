a88ad76adec730609990f2600714752b327575e8
Loli Delgado <madodela@gmail.com>
2016-08-30 08:42:46 -0500

    Code refactor, names changed, specific functions created

--- src/environment-canvas.js
+++ src/environment-canvas.js
@@ -1,103 +1,104 @@
 'use strict';
 
 var cellsPositions = [],
-	order = 0,
+	gridSize = 0,
 	canvas;
 
 function createGrid() {
 
-	order = document.getElementsByName('order')[0].value;
-	if(!!order && order > 0) {
+	gridSize = document.getElementById('grid-size').value;
+	if(!!gridSize && gridSize > 0) {
 		cellsPositions = [];
-		drawCanvas(order);
+		drawCanvas();
 	} else {
-		alert("Please type valid order");
+		alert("Please type valid gridSize");
 	}
 }
 
 function drawCanvas() {
 	var elem = document.getElementById('environment'),
-  	distance = order * 33.75;
-  	canvas = elem.getContext('2d');
-	
-	elem.width = Math.round(distance) + 15;
-	elem.height = Math.round(distance) + 15;
+	  	gridHeightWidth = gridSize * 33.75,
+	  	elemHeightWidth = Math.round(gridHeightWidth) + 15;
+  	
+	elem.width = elemHeightWidth;
+	elem.height = elemHeightWidth;
 	elem.addEventListener('click', changeCellStatus);
  
+ 	canvas = elem.getContext('2d');
   	canvas.fillStyle = "#ccc";
+	canvas.clearRect(10, 10, gridHeightWidth, gridHeightWidth);
 
-	//(x, y, distance_x, distance_y)
-	canvas.clearRect(10, 10, distance, distance);
-  	canvas.strokeRect(10, 10, distance, distance);
-
-  	drawCells(canvas);
+  	drawCells();
 }
 
 function changeCellStatus(event) {
 	var x = event.offsetX,
 		y = event.offsetY;
 
-	for(let index = 0, len = cellsPositions.length; index <= len ; index++) {
-		let cell = cellsPositions[index],
-			right = cell.x + cell.size,
+	cellsPositions.forEach(function (cell) {
+		let right = cell.x + cell.cellSize,
 			left = cell.x,
 			top = cell.y,
-			bottom = cell.y + cell.size,
+			bottom = cell.y + cell.cellSize,
 			isSelected = right >= x && left <= x && bottom >= y && top <= y;
-
+		
 		if(isSelected) {
 			cell.isAlive = !cell.isAlive;
 			changeCellCanvas(cell);
 			return;
 		}
-	}
+	});
 }
 
 function changeCellCanvas(cell){
 	canvas.fillStyle = cell.isAlive ? '#0f0' : '#ccc';
-	canvas.fillRect(cell.x, cell.y, cell.size, cell.size);
+	canvas.fillRect(cell.x, cell.y, cell.cellSize, cell.cellSize);
 }
 
-function drawCells(canvas) {
-	var size = 30,
+function drawCells() {
+	var cellSize = 30,
 		y = 15,
-		cellsQuantity = order;
+		cellsQuantity = gridSize;
 
 	for(; cellsQuantity>0; cellsQuantity --) {
-		let cells = order,
+		let cells = gridSize,
 			x = 15;
 		for(; cells>0; cells --) {
-			cellsPositions.push({x: x, y: y, size: size, isAlive: false});
-			canvas.fillRect(x, y, size, size);
+			cellsPositions.push({x: x, y: y, cellSize: cellSize, isAlive: false});
+			canvas.fillRect(x, y, cellSize, cellSize);
 			x += 31;
 		}
 		y += 31;
 	}
-	document.getElementById('next-button').className = '';
+	document.getElementById('next-gen-btn').className = '';
 }
 
 function createNewGeneration() {
 
-	var cellStates = [],
-		cells = [],
-		environment = new Environment(order),
+	var environment = new Environment(gridSize),
 		newGeneration = [];
-
-	cellStates = cellsPositions.map(function(cell) {
-		return new Cell(cell.isAlive);
-	});
-
-	while(cellStates.length) {
-	    cells.push(cellStates.splice(0, order));
-	}
-	environment.setCells(cells);
-
+	
+	environment.setCells(getCellsArray());
 	newGeneration = environment.nextGeneration();
 	// flatten array
 	newGeneration = [].concat.apply([], newGeneration);
-	
+	//changing cell color according to new status
 	cellsPositions.forEach(function(cell, index) {
 		cell.isAlive = newGeneration[index].isAlive;
 		changeCellCanvas(cell);
 	});
+}
+
+function getCellsArray() {
+	var cells = [],
+		cellStates = [];
+	//Get cell's status of life in a plain array
+	cellStates = cellsPositions.map(function(cell) {
+		return new Cell(cell.isAlive);
+	});
+	//Making a two-dimensional array
+	while(cellStates.length) {
+	    cells.push(cellStates.splice(0, gridSize));
+	}
+	return cells;
 }
